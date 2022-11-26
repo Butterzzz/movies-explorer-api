b2b1ce4d268f6e0e1bf31558ae97c8e9e6ced88d
Grinchenko Sergei <butters.blaga@gmail.com>
2022-11-27 00:36:53 +0900

    feat: implement logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3002 } = process.env;
 const app = express();
@@ -8,9 +9,14 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(express.json());
+app.use(requestLogger); // логгер запросов
 
 // подключаем мидлвары, роуты и всё остальное...
 app.use(router);
+
+app.use(errorLogger); // логгер ошибок
+// тут будет обработчик ошибок celebrate
+// тут будет централизованный обработчик ошибок
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
